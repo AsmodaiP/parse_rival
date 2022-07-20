fbb3d8c7fde6d1b3b937127629ec5096c6fb910b
Dmitriy Pereguda <asmoday.p@gmial.com>
2022-07-20 16:26:32 +0300

    add type checking and work with multiple products

--- bot.py
+++ bot.py
@@ -1,45 +1,47 @@
 import datetime
+from typing import Dict, Union
 from parsing_rival import get_info
 import telegram
 import logging
 import os
-import sys
 import time
-import random
 from dotenv import load_dotenv
 load_dotenv()
 
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 bot = telegram.Bot(token=os.environ['TELEGRAM_TOKEN'])
 
 
-def format_message(info):
+def format_message(info: Dict[str, str]) -> str:
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
     message = (f'<b>{today}</b> \n Описание:\n {info["description"]} \n\n'
                f'<b>{yesterday}</b> \nОписание:\n {info["yesterday_description"]} \n\n'
-               f'Разница:\n {info["difference"]} \n\n')
-               
+               )
+    if len(info['difference']) > 41:
+        message += f'Разница: \n {info["difference"]} \n\n'
+    else:
+        message += f'Разницы нет \n\n'
     if len(info) > 3:
         message += 'Характеристики:\n'
         for key in info:
             if key == 'description' or key == 'yesterday_description' or key == 'difference':
                 continue
             message += f'{key}: {info[key]}\n'
-    else: 
+    else:
         message += 'Характеристики не изменились'
     return message.replace('**', '')
 
 
-def send_long_message(chat_id, message):
+def send_long_message(chat_id: Union[int, str], message: str) -> None:
     for i in range(0, len(message), 4096):
         bot.sendMessage(chat_id=chat_id, text=message[i:i+4096], parse_mode=telegram.ParseMode.HTML)
         time.sleep(1)
 
-def send_long_message_to_all(message):
+
+def send_long_message_to_all(message: str) -> None:
     for chat_id in os.environ['CHAT_IDS'].split(','):
         try:
             send_long_message(chat_id, message)
@@ -47,8 +49,16 @@ def send_long_message_to_all(message):
             logging.error(f'Error while sending message to chat {chat_id} ' + str(e))
             continue
 
+
 if __name__ == '__main__':
-    info = get_info(os.environ['PRODUCT_ID'])
-    print(info)
-    if info:
-        send_long_message_to_all(format_message(info))
+    for product_id in os.environ['PRODUCT_IDS'].split(','):
+        print(f'Parsing product {product_id}')
+        try:
+            info = get_info(product_id)
+            if info is None:
+                continue
+            message = format_message(info)
+            send_long_message_to_all(message)
+        except Exception as e:
+            logging.error(f'Error while parsing product {product_id} ', exc_info=True)
+            continue
--- parsing_rival.py
+++ parsing_rival.py
@@ -1,35 +1,43 @@
 
 import datetime
 import json
-from typing import Dict, Union
+from typing import Dict, Optional, Union
 import requests
 import os
 
 
-def get_characteristics(id: Union[str, int]) -> Dict[str, str]:
+def create_path_if_not_exists(path: str):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def get_characteristics(id: Union[str, int]) -> Dict[str, Union[str, Dict[str, str]]]:
     url = f"https://wbx-content-v2.wbstatic.net/ru/{id}.json"
     return requests.get(url).json()
 
 
 def get_rival_info(id: Union[str, int]) -> Dict[str, str]:
     characteristics = get_characteristics(id)
-    result = {'description': characteristics['description']}
+    result: Dict[str, str] = {'description': characteristics['description']}
     for option in characteristics['options']:
         result[option['name']] = option['value']
     return result
+
 
 def replace_specific_words_to_bold(words: set, text: str) -> str:
     for word in words:
         text = text.replace(word, f'<b>{word}</b>')
     return text
 
-def get_info(id: Union[str, int]) -> Dict[str, str]:
+
+def get_info(id: Union[str, int]) -> Optional[Dict[str, str]]:
+    create_path_if_not_exists('data')
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    rival_info  = get_rival_info(61637961)
-    with open(f'{today}_{id}.json', 'w') as f:
+    rival_info = get_rival_info(id)
+    with open(f'data/{today}_{id}.json', 'w') as f:
         json.dump(rival_info, f, indent=4, ensure_ascii=False)
-    if os.path.exists(f'{yesterday}_{id}.json'):
+    if os.path.exists(f'data/{yesterday}_{id}.json'):
         yesterday_rival_info = json.load(open(f'{yesterday}_{id}.json'))
         result = {}
         for key in rival_info:
@@ -37,21 +45,21 @@ def get_info(id: Union[str, int]) -> Dict[str, str]:
                 continue
             if rival_info[key] != yesterday_rival_info[key]:
                 result[key] = (f'{yesterday_rival_info[key]} ➡️ {rival_info[key]}')
-        
-        if rival_info['description'] != yesterday_rival_info['description']:
-
+        if rival_info != yesterday_rival_info:
             added = set(rival_info["description"].split(' ')) - \
                 set(yesterday_rival_info["description"].split(' '))
             deleted = set(yesterday_rival_info["description"].split(' ')) - \
                 set(rival_info["description"].split(' '))
 
             rival_info['description'] = replace_specific_words_to_bold(added, rival_info['description'])
-            yesterday_rival_info['description'] = replace_specific_words_to_bold(deleted, yesterday_rival_info['description'])
-            result['description'] =rival_info['description']
+            yesterday_rival_info['description'] = replace_specific_words_to_bold(
+                deleted, yesterday_rival_info['description'])
+            result['description'] = rival_info['description']
             result['yesterday_description'] = yesterday_rival_info['description']
             result['difference'] = f"Добавились слова в описание {', '.join(added) }, удалились {', '.join(deleted)}"
-
             return result
+    return None
+
 
 if __name__ == "__main__":
     pass
